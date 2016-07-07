d69751fa9711b80f59130cce95627cd99256ec68
R-Carver <davidmakowski00@gmail.com>
2016-07-07 10:46:18 +0200

    Add files via upload

---
+++ SignalRMvcTests/SignalR/Messenger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Utilities.SignalR
+{
+    public class Messenger
+    {
+        private readonly static Lazy<Messenger> _instance = new Lazy<Messenger>(() => new Messenger(GlobalHost.ConnectionManager.GetHubContext<MessengerHub>().Clients));
+
+        private Messenger(IHubConnectionContext<dynamic> clients)
+        {
+            Clients = clients;
+        }
+
+        public static Messenger Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        private IHubConnectionContext<dynamic> Clients { get; set; }
+
+        public void BroadCastNewContract()
+        {
+            Clients.All.showNewContractMessage();
+        }
+
+    }
+}
---
+++ SignalRMvcTests/SignalR/MessengerHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebApplication1.Utilities.SignalR
+{   
+    [HubName("MessengerHub")]
+    public class MessengerHub : Hub
+    {
+        private readonly Messenger _messenger;
+
+        public MessengerHub() : this(Messenger.Instance) { }
+
+        public MessengerHub(Messenger messenger)
+        {
+            _messenger = messenger;
+        }
+
+    }
+}
